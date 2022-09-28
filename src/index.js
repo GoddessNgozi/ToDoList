e2207c50f5d2afe09e5937e844893a12ad2c6538
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-09-28 01:37:31 +0100

    Add toDo list structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 import _ from 'lodash';
 import './style.css';
+
+const list = document.querySelector('.list');
+const taskForm = document.querySelector('.taskForm');
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    function addList() {
+        taskForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const desc = document.querySelector('.description');
+            const description = desc.value;
+            const completed = 'false';
+            const index = tasks.length;
+            const task = {
+                description,
+                completed,
+                index,
+            };
+            tasks.push(task);
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            console.log(tasks);
+            taskForm.reset();
+            tasks.forEach((task) => {
+                list.innerHTML += `<li><input type="checkbox" class="checker"><input type="text" class="list-input" value="${task.description}" disabled><i class="fa fa-ellipsis-vertical"></i></li>`;
+            });
+    });
+}
+addList();
+
+document.addEventListener('DOMContentLoaded', () => {
+    tasks.forEach((task) => {
+        list.innerHTML += `<li><input type="checkbox" class="checker"><input type="text" class="list-input" value="${task.description}" disabled><i class="fa fa-ellipsis-vertical"></i></li>`;
+    });
+});
