8e1edab21016ad0e4cc8a2f5ba7349bea2765b0e
goddessngozi <51258047+NgozikaNwocha@users.noreply.github.com>
2022-09-28 22:05:03 +0100

    Add functions to add, remove and edit tasks

--- src/index.js
+++ src/index.js
@@ -1,30 +1,70 @@
 import './style.css';
 
 const list = document.querySelector('.list');
-const tasks = [
-  {
-    description: 'Learn coding',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Do yoga',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Have dinner with family',
-    completed: false,
-    index: 3,
-  },
-];
+const desc = document.querySelector('.description');
+const form = document.querySelector('.toDoForm');
+let toDos = JSON.parse(localStorage.getItem('toDos')) || [];
 
-function addList(tasks) {
+const addToDo = () => {
+  form.addEventListener('submit', () => {
+    const toDo = {
+      description: desc.value,
+      completed: false,
+      index: toDos.length + 1,
+    };
+    toDos.push(toDo);
+    localStorage.setItem('toDos', JSON.stringify(toDos));
+  });
+  return toDos;
+};
+
+const displayToDo = (toDos) => {
   let markup = '';
-  tasks.forEach((task) => {
-    markup += `<li><input type="checkbox" class="checker"><input type="text" class="list-input" value="${task.description}" disabled><i class="fa fa-ellipsis-vertical"></i></li>`;
+  toDos.forEach((toDo) => {
+    markup += `<li class="doList"><input type="checkbox" class="checker"><input type="text" class="list-input" value="${toDo.description}"><i class="drag fa fa-ellipsis-vertical"></i><i id="${toDo.index}" class="trash fa-solid fa-trash-can"></i></li>`;
   });
   list.innerHTML = markup;
+};
+
+const removeToDo = () => {
+  const trashes = document.querySelectorAll('.trash');
+  trashes.forEach((trash) => {
+    trash.addEventListener('mousedown', (e) => {
+      e.target.parentElement.remove();
+      toDos = toDos.filter((toDo) => toDo.index !== +e.target.id);
+      localStorage.setItem('toDos', JSON.stringify(toDos));
+    });
+  })
+};
+
+const editTask = () => {
+  const edits = document.querySelectorAll('.list-input');
+  edits.forEach((edit, indexy) => {
+    edit.addEventListener('focusin', (e) => {
+      e.target.parentElement.style.background = '#faf8b1';
+      e.target.parentElement.lastChild.style.display = 'block';
+      e.target.nextSibling.style.display = 'none';
+    });
+    edit.addEventListener('focusout', (e) => {
+      e.target.parentElement.style.background = '#ffffff';
+      e.target.parentElement.lastChild.style.display = 'none';
+      e.target.nextSibling.style.display = 'block';
+    });
+    edit.addEventListener('change', (e) => {
+      e.target.parentElement.style.background = '#ffffff';
+      edit.placeholder = edit.value;
+      toDos.forEach((toDo, index) => {
+        if (indexy === index) {
+          //toDos[index].description = edit.value;
+          toDo.description = edit.value;
+          localStorage.setItem('toDos', JSON.stringify(toDos));
+        }
+      });
+    })
+  })
 }
 
-addList(tasks);
+addToDo();
+displayToDo(toDos);
+removeToDo();
+editTask();
